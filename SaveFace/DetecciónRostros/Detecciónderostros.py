68cb96cddcbc2a0b4e1b5347c99b83e6d9562283
Matias <matias.olavegonzalez@gmail.com>
2021-10-29 01:48:21 -0300

    reconocimiento facial 1.1
    3 metodos distintos de reconocimiento, soluciones de bugs, modificacion en lugar de guardado de reconocimiento B y C, reconocimiento A no sé a podido modificar lugar de almacenamiento

--- SaveFace/DetecciónRostros/Detecciónderostros.py
+++ SaveFace/DetecciónRostros/Detecciónderostros.py
@@ -16,74 +16,41 @@ from matplotlib import pyplot
 from matplotlib.patches import Rectangle
 from matplotlib.patches import Circle 
 from mtcnn.mtcnn import MTCNN
-#import numpy as np
+import mediapipe as mp
 import os
-#import imutils
 
 
-def reconocimientoA():
-    #captura de rostro
-    cap = cv2.VideoCapture(0) #seleccion de camara
-    while(True): 
-        ret,frame=cap.read() # leemos el video 
-        cv2.imshow('Rostro',frame) #mostramos el video en pantalla 
-        if cv2.waitKey(1) == 27: # cuando se presiona " escape" se para el video 
-            break 
-    cv2.imwrite('Rostro.jpg',frame)
-    cap.release() # cerramos se tiene que cambiar por el boton 
-    cv2.destroyAllWindows() #cerrar la ventana de la camara pero hay que cambiarlo al boton que apaga la camara
 
-          
-    def dibujo(img, lista_resultados):
-        print(caras)
-        # leemos la imagen con la libreria matplotlib
-        imagen= pyplot.imread(img)
-        #Ploteamos la imagen
-        pyplot.imshow(imagen)
-        #obtenemos los ejes polares para dibujar
-        ax= pyplot.gca()
-        #ploteamos los rectangulos con las coordenadas que nos entrega 'box'
-        for result in lista_resultados:
-            #obtenemos las coordenadas
-            x, y, ancho, alto= result['box']
-            #creamos el rectangulo
-            rect=Rectangle((x,y), ancho, alto, fill= False, color= 'green')
-            #Dibujamos la caja
-            ax.add_patch(rect)
-            #ahora vamos a dibujar los ojos, nariz y boca puntos clave del rostro
-            for puntos, value in result['keypoints'].items():
-                #creamos los circulos
-                dot= Circle(value, radius= 4, color='green')
-                ax.add_patch(dot)
-#######################para exportar#################################################################
-        #ahora vamos a exportar los pixeles que pertenecen a los rostros con el fin de usarlos en otro sistema
-        for i in range(len(lista_resultados)):
-            #obtenemos las coordenadas
-            x1, y1, ancho1, alto1 = lista_resultados[i]['box']
-            x2, y2 = x1 + ancho1, y1 + alto1
-            #definimos el subplot
-            pyplot.subplot(1, len(lista_resultados),i+1)
-            pyplot.axis('off')
-            #ploteamos las caras
-            pyplot.imshow(imagen[y1:y2, x1:x2])
-#####################################################################################################
-        #ploteamos la imagen con el dibujo del rectangulo
-        pyplot.show()
-   #leemos la imagen con la libraria matplotlib
-    img = 'Rostro.jpg'
-    pixeles= pyplot.imread(img)
+def reconocimientoA(nombrePersona):
+    direccion= 'C:/Users/Matias/Documents/Repositorios/Proyecto Metodologia SaveFace/G2_Metodologia_2_2021/SaveFace/Guardar informacion de rostros'
+    nombreCara= nombrePersona
+    direccionFinal = direccion + '/' + nombreCara
+    if not os.path.exists(direccionFinal):
+        print('Carpeta creada: ', direccionFinal)
+        os.makedirs(direccionFinal)
+        
+    face_detection= mp.solutions.face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5)
+    cap= cv2.VideoCapture(0)
     
-    #creamos el detector usando los valores predeterminados
-    detector = MTCNN()
-    
-    #detectamos las caras en la imagen
-    caras= detector.detect_faces(pixeles)
-    
-    #llama la funcion que ya se creo para dibujar los rectangulos
-    dibujo(img, caras)
+    while True:
+        ret, im= cap.read()
+        
+        if not ret:
+            break
+        im2=cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+        results= face_detection.process(im2)
+        
+        if results.detections:
+            for detection in results.detections:
+                mp.solutions.drawing_utils.draw_detection(im, detection)
+                cv2.imwrite(direccionFinal + '/rostro_{}.jpg'.format(nombreCara),im)
+        cv2.imshow('imagen',im)
+        if cv2.waitKey(1) == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
-    
-#reconocimientoA()
+#reconocimientoA("matias")
 
 def reconocimientoB(nombrePersona): ## podriamos pedirselo al usuario examinandolo (?)
    
@@ -93,8 +60,10 @@ def reconocimientoB(nombrePersona): ## podriamos pedirselo al usuario examinando
     if not os.path.exists(direccionFinal):
         print('Carpeta creada: ', direccionFinal)
         os.makedirs(direccionFinal)
+        
     faceClassif= cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     cap=cv2.VideoCapture(0)
+    
     while True:
         _,img=cap.read()
         gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -116,19 +85,26 @@ def reconocimientoB(nombrePersona): ## podriamos pedirselo al usuario examinando
 #reconocimientoB()# hay que pasar el nombre de la persona a la cual se le identifico el rostro
 
 
-def reconocimientoC(NombreUsuario):
+def reconocimientoC(nombrePersona):
+     direccion= 'C:/Users/Matias/Documents/Repositorios/Proyecto Metodologia SaveFace/G2_Metodologia_2_2021/SaveFace/Guardar informacion de rostros'
+     nombreCara= nombrePersona
+     direccionFinal = direccion + '/' + nombreCara
+     if not os.path.exists(direccionFinal):
+        print('Carpeta creada: ', direccionFinal)
+        os.makedirs(direccionFinal)
      #captura de rostro
+     
      cap = cv2.VideoCapture(0)
      while(True):
          ret,frame = cap.read()
          cv2.imshow('Deteccion Facial',frame)
          if cv2.waitKey(1) == 27:
              break
-     usuario_img = NombreUsuario
+     usuario_img = nombrePersona
      cv2.imwrite(usuario_img + ".jpg",frame)
      cap.release()
      cv2.destroyAllWindows()
-         #nomreUsuario.delete(0, END) # se ocupa para limpiar la variable, posible utilizacion en pantalla.py
+     #nomreUsuario.delete(0, END) # se ocupa para limpiar la variable, posible utilizacion en pantalla.py
          
      def Identificar_cara(img, lista_resultados):
         data = pyplot.imread(img)
@@ -138,15 +114,18 @@ def reconocimientoC(NombreUsuario):
             pyplot.subplot(1, len(lista_resultados), i+1)
             pyplot.axis('off')
             cara_reg = data[y1:y2, x1:x2]
-            cara_reg = cv2.resize(cara_reg, (150,200), interpolation = cv2.INTER_CUBIC)
-            cv2.imwrite(usuario_img+".jpg",cara_reg)
+            cara_reg = cv2.resize(cara_reg, (150,150), interpolation = cv2.INTER_CUBIC)
+            cv2.imwrite(direccionFinal+'/rostro_{}.jpg'.format(nombreCara),cara_reg)
             pyplot.imshow(data[y1:y2, x1:x2])
         pyplot.show()
+     
      img= usuario_img+".jpg"
      pixeles= pyplot.imread(img)
      detector= MTCNN()
      caras= detector.detect_faces(pixeles)
      Identificar_cara(img,caras)
+     
+     
 #reconocimientoC("matias")
          
 #face_cascade= cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
