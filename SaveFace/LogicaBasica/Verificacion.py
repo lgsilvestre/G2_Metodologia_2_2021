80a71a16c03c16caa70862a4a0b7e232e53c09c0
Cristian Fuentes <crfuentes19@alumnos.utalca.cl>
2021-11-29 19:13:25 -0400

    Verificaciones varias
    Se verifica que el correo sea válido, entre otras cosas

--- SaveFace/LogicaBasica/Verificacion.py
+++ SaveFace/LogicaBasica/Verificacion.py
@@ -26,20 +26,36 @@ def verificarUsuario(usuario, contraseña):
     return 'Error'   
         
 
-# cerrar el archivo
-
-#para importar las funciones a otro fichero se utiliza " from funciones import verificarUsuario, verificarContraseña "
-
-#print (archivo.read())
-
-#linea = archivo.readlines()# leer una linea, y luego avanza
-#print(linea) # mostrar por pantalla la linea
-
-# un ciclo que termina cuando una linea está vacia ( que es 0)
-#while len(linea)>0:
-#    print(linea)# el print hace el salto de linea y se puede cambiar con (end =' ') despues de linea en el print
-#    linea=archivo.readline()
-
-#funciona mas directo con el for
-#for linea in archivo:
-#    print(linea, end=' ')
+def verificarCorreo(Correo):
+    signos = ['.','_','-']
+    numeros = ['0','1','2','3','4','5','6','7','8','9']
+    dominios = ['gmail', 'hotmail', 'live']
+    minusculas = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z']
+    mayusculas = []
+    extenciones = ['com','cl']
+    
+    for x in minusculas:
+        mayusculas.append(x.upper())
+    
+    if Correo.find('@') != -1:
+        nuevo_email = Correo.split('@')
+        usuario = nuevo_email[0]
+        resto = nuevo_email[1]
+        continuacion = resto.split('.')
+        dominio = continuacion[0]
+        terminacion = continuacion[1]
+        for x in usuario:
+            if x in signos or x in numeros or x in minusculas or x in mayusculas:
+                if dominio in dominios:
+                    if terminacion in extenciones:
+                        return True
+                    else:
+                        return True
+                else:
+                    return True
+            else:
+                return False
+    else:
+        return False
+    
+    return False
